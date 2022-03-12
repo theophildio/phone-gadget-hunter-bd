b8fee4bba274321673e48bbc7114dcc773da9ab1
theophildio <dtheo.sangma@gmail.com>
2022-03-13 00:56:13 +0600

    Added pop on phone details

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,53 +112,60 @@ const close = () => {
 };
  // Show Phone Details 
 const getPhoneDetails = details => {
-  // console.log(details);
+  const detailsClose = getId('phone-details');
+  detailsClose.style.display = 'block';
   const showPhoneDetails = getId('phone-details');
   showPhoneDetails.textContent = '';
   const div = document.createElement('div');
-  div.innerHTML = `<div class="card mb-3 p-2 shadow-sm">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${details.name}</h5>
-          <p class="card-text"><small>Brand:</small> ${details.brand}</p>
-          <p class="card-text"><small>${details.releaseDate ? details.releaseDate : 'No release date found'}</small></p>
-          <div class="phone-specification">
-            <h5 class="card-title">Main Specification</h5>
-            <div class="details">
-              <ul>
-                <li>Storage: <small>${details.mainFeatures.storage}</small></li>
-                <li>Display: <small>${details.mainFeatures.displaySize}</small></li>
-                <li>Chip Set: <small>${details.mainFeatures.chipSet}</small></li>
-                <li>Memory: <small>${details.mainFeatures.memory}</small></li>
-              </ul>
-            </div>
-            <h5 class="card-title">Sensors</h5>
-            <div class="details">
-              <ul>
-                <li>Sensors: <small>${details.mainFeatures.sensors}</small></li>
-              </ul> 
-            <div>
-              <h5 class="card-title">Other Specification</h5>
-              <div class="details">
-                <ul>
-                  <li>WLAN: <small>${details.others ? details.others.WLAN : 'N/A'}</small></li>
-                  <li>Bluetooth: <small>${details.others ? details.others.Bluetooth : 'N/A'}</small></li>
-                  <li>GPS: <small>${details.others ? details.others.GPS : 'N/A'}</small></li>
-                  <li>NFC: <small>${details.others ? details.others.NFC : 'N/A'}</small></li>
-                  <li>Radio: <small>${details.others ? details.others.Radio : 'N/A'}</small></li>
-                  <li>USB: <small>${details.others ? details.others.USB : 'N/A'}</small></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>`;
+  div.innerHTML = `<div class="card mb-3 p-2 shadow-lg">
+                      <div class="row g-0">
+                        <div class="col-md-4">
+                          <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                          <div class="card-body">
+                            <h5 class="card-title">${details.name}</h5>
+                            <button onclick="detailsClose()" id="details-close" class="btn btn-danger">X</button>
+                            <p class="card-text"><small>Brand:</small> ${details.brand}</p>
+                            <p class="card-text"><small>${details.releaseDate ? details.releaseDate : 'No release date found'}</small></p>
+                            <div class="phone-specification">
+                              <h5 class="card-title">Main Specification</h5>
+                              <div class="details">
+                                <ul>
+                                  <li>Storage: <small>${details.mainFeatures.storage}</small></li>
+                                  <li>Display: <small>${details.mainFeatures.displaySize}</small></li>
+                                  <li>Chip Set: <small>${details.mainFeatures.chipSet}</small></li>
+                                  <li>Memory: <small>${details.mainFeatures.memory}</small></li>
+                                </ul>
+                              </div>
+                              <h5 class="card-title">Sensors</h5>
+                              <div class="details">
+                                <ul>
+                                  <li>Sensors: <small>${details.mainFeatures.sensors}</small></li>
+                                </ul> 
+                              <div>
+                                <h5 class="card-title">Other Specification</h5>
+                                <div class="details">
+                                  <ul>
+                                    <li>WLAN: <small>${details.others ? details.others.WLAN : 'N/A'}</small></li>
+                                    <li>Bluetooth: <small>${details.others ? details.others.Bluetooth : 'N/A'}</small></li>
+                                    <li>GPS: <small>${details.others ? details.others.GPS : 'N/A'}</small></li>
+                                    <li>NFC: <small>${details.others ? details.others.NFC : 'N/A'}</small></li>
+                                    <li>Radio: <small>${details.others ? details.others.Radio : 'N/A'}</small></li>
+                                    <li>USB: <small>${details.others ? details.others.USB : 'N/A'}</small></li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>`;
   showPhoneDetails.appendChild(div);
 };
 
+// Close phone details pop up
+const detailsClose = () => {
+  const detailsClose = getId('phone-details');
+  detailsClose.style.display = 'none';
+}
